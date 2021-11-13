d8443dd8e0c0dc5ea5dd11ffe9cfd84b95b67d6e
Ricardo CF <ri_ca_fa@hotmail.com>
2021-11-13 10:23:05 -0300

    add level 2

--- js/app.js
+++ js/app.js
@@ -1,18 +1,31 @@
-const MAX_NUMBER = 5;
-const MIN_NUMBER = 0;
+  
+var nivel      = 1;
+var max_number = 5; //numero maximo de diferença entre os números.
+var min_number = 0;
 
 window.addEventListener('load', (event) => {
     startIt();
 });
 
 function getRandomInt() {
-    return Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER + 1)) + MIN_NUMBER;
+    return Math.floor(Math.random() * (max_number - min_number + 1)) + min_number;
 };
 
 function startIt(){
+    if(nivel ==1){     
+        window.max_number = 5;
+        window.min_number = 0;   
+    }
+    else if (nivel == 2){
+        window.max_number = 10;
+        window.min_number = 5;
+    }
     num1_1.value = 0;
     while(parseInt(num1_1.value)==0){
         num1_1.value = getRandomInt();
+    }
+    while(parseInt(num2_1.value)<min_number){
+        num2_1.value = getRandomInt();
     }
     num2_1.value = '';
     tot_1 .value = parseInt(num1_1.value)+getRandomInt();
